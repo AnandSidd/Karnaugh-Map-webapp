6b0092e04fc05b73cdce673d31b692d1695ede73
AnandSidd <siddharth.asrivastav.ece19@itbhu.ac.in>
2021-04-24 18:13:04 +0530

    Updates

---
+++ precache-manifest.8d79fba821170b85712c532f87da6009.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b7cbef62cb0f0a5d17ddc3e4f84281d",
+    "url": "/KMapWebApp/index.html"
+  },
+  {
+    "revision": "e17d1e6998da46e6ae30",
+    "url": "/KMapWebApp/static/css/main.e91acf3c.chunk.css"
+  },
+  {
+    "revision": "79eb4cf90be613cf5039",
+    "url": "/KMapWebApp/static/js/2.fb6c3cf8.chunk.js"
+  },
+  {
+    "revision": "727196b53be8768c78b2ea60f81f0ea1",
+    "url": "/KMapWebApp/static/js/2.fb6c3cf8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e17d1e6998da46e6ae30",
+    "url": "/KMapWebApp/static/js/main.9456d777.chunk.js"
+  },
+  {
+    "revision": "739cccfd4543a00b030c",
+    "url": "/KMapWebApp/static/js/runtime-main.9741cec3.js"
+  }
+]);
